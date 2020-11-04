ec462d33ca9878b22ff4b433c24c7b1ca2a6c72c
Skyler Tesar <skyler-tesar@uiowa.edu>
2020-11-04 13:29:05 -0600

    worked on project more, gotta pull now

--- java_examples/player_search_maven_app/src/main/java/com/player/dao/dbutil/PlayerSearchQueries.java
+++ java_examples/player_search_maven_app/src/main/java/com/player/dao/dbutil/PlayerSearchQueries.java
@@ -6,4 +6,5 @@ public class PlayerSearchQueries {
 	public static final String GETALLPLAYERS = "select id,name,age,gender,teamname,contact from roc_revature.player";
 	public static final String GETPLAYERSBYGENDER = "select id,name,age,teamname,contact from roc_revature.player where gender=?";
 	public static final String GETPLAYERSBYNAME = "select  id, age, gender, teamname, contact from roc_revature.player where name=?";
+	public static final String GETPLAYERSBYAGE = "select id,name,age,gender,teamname,contact from roc_revature.player where age=?";
 }
